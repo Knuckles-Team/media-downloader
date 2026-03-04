3ddd7f5a22afa9bd2a620ee33e9a4ce767139b85
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-03 22:56:44 -0600

    Bump version: 2.2.23 → 2.2.24

--- media_downloader/mcp.py
+++ media_downloader/mcp.py
@@ -31,7 +31,7 @@ from agent_utilities.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "2.2.23"
+__version__ = "2.2.24"
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
